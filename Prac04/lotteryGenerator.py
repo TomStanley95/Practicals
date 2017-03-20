fb8495fd897ea633258576710f04cfd4877addbb
TomStanley95 <tom.stanley@my.jcu.edu.au>
2017-03-20 22:45:16 +1000

    completed loterryGenerator

--- Prac04/lotteryGenerator.py
+++ Prac04/lotteryGenerator.py
@@ -1,10 +1,21 @@
 __author__ = 'Tom Stanley'
 import random
-number_of_picks = int(input("How many quick picks?"))
-# Ask the user for the amount of quick picks to generate, then creates that many emtpy lists
+number_of_picks = int(input("How many quick picks? "))
+# Ask the user for the amount of quick picks to generate.
 quick_picks = []
 for i in range(0, number_of_picks):
-    quick_picks.append(0)
-print(quick_picks)
+    current_pick = []
+    for n in range(0, 6):
+        random_number = random.randrange(0, 46)
+        # current_pick.append(random.randrange(0, 46))
+        # generate a random number between 0 and 46, then checks if it already exists, if so generate a new number.
+        while random_number in current_pick:
+            random_number = random.randrange(0, 46)
+        current_pick.append(random_number)
+        current_pick.sort()
 
-random.randrange(0, 45)
+    quick_picks.append(current_pick)
+
+for i in range(0, number_of_picks):
+    print("{}".format(str(quick_picks[i]).strip('[]').replace(',', " ")))
+

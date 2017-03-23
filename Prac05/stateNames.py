d81ef5d4e236965a296970a09d8d3ee80c72da94
TomStanley95 <tom.stanley@my.jcu.edu.au>
2017-03-23 16:07:42 +1000

    Added for loop to print all of dictionary

--- Prac05/stateNames.py
+++ Prac05/stateNames.py
@@ -8,7 +8,7 @@ File needs reformatting
 
 STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-# print(STATE_NAMES)
+print(STATE_NAMES)
 
 state = input("Enter short state: ").upper()
 while state != "":
@@ -17,3 +17,6 @@ while state != "":
     else:
         print("Invalid short state")
     state = input("Enter short state: ").upper()
+
+for state in STATE_NAMES:
+    print("{:<4s} is {:s}".format(state, STATE_NAMES[state]))

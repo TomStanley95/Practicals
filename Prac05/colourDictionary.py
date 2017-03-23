9842fec32290c713aab4a9b1c3b591963ccbd46e
TomStanley95 <tom.stanley@my.jcu.edu.au>
2017-03-23 16:24:39 +1000

    Added color dictionary

---
+++ Prac05/colourDictionary.py
@@ -0,0 +1,10 @@
+__author__ = 'Tom Stanley'
+HEX_COLORS_DICTIONARY = {"Aliceblue": "#f0f8ff", "Blue": "#0000ff", "Cyan": "#00ffff", "Gold": "#ffd700",
+                         "Gray": "#bebebe", "Green": "#00ff00", "Hotpink": "#ff69b4", "Purple": "#a020f0",
+                         "Red": "#ff0000", "Yellow": "#ffff00"}
+
+# print(HEX_COLORS_DICTIONARY)
+
+color = input("Enter color:").lower().capitalize().strip().replace(" ", "")
+# print(color)
+

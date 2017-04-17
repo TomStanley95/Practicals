e6cb1804048ac07547bcdb9130718b07ee1c21dd
TomStanley95 <tom.stanley@my.jcu.edu.au>
2017-04-17 20:19:45 +1000

    finished stringCount

---
+++ Prac05/stringCount.py
@@ -0,0 +1,18 @@
+__author__ = 'Tom Stanley'
+stringDict = {}
+string = "Hello my my my name is rosie".split()
+# string = input("Enter your string:").lower().split
+for word in string:
+    if word in stringDict:
+        stringDict[word] += 1
+    else:
+        stringDict[word] = 1
+longest_word_len = 0
+for key in stringDict:
+    current_word_len = len(key)
+    if current_word_len > longest_word_len:
+        longest_word_len = current_word_len
+sorted_string_dict = (sorted(stringDict))
+print(sorted_string_dict)
+for word in sorted_string_dict:
+    print("{:{}} : {}".format(word, longest_word_len, stringDict.get(word)))

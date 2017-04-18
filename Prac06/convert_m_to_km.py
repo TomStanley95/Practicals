d9a04242e6ec4dcc7d95cf146cce9174d630fb32
TomStanley95 <tom.stanley@my.jcu.edu.au>
2017-04-18 21:07:53 +1000

    Stage 2 of convert_m_to_km

--- Prac06/convert_m_to_km.py
+++ Prac06/convert_m_to_km.py
@@ -11,18 +11,24 @@ class ConvertMiles(App):
     def build(self):
         self.title = 'Convert Miles to Kilometres'
         self.root = Builder.load_file('convert_m_to_km.kv')
-        Window.size = (300, 300)
+        Window.size = (500, 300)
         return self.root
 
     def handle_increment(self, increment):
-        new_value = float(self.root.ids.input_number.text) + int(increment)
+        new_value = self.get_valid_miles() + increment
         self.root.ids.input_number.text = str(new_value)
+        self.convert_miles()
 
     def convert_miles(self):
-        miles = float(self.root.ids.input_number.text)
+        miles = self.get_valid_miles()
         km = miles * MILES_TO_KM
-        self.root.ids.output_label.text = str(km)
+        self.root.ids.output_label.text = "{} miles is {}".format(self.root.ids.input_number.text, km)
 
-
+    def get_valid_miles(self):
+        try:
+            value = float(self.root.ids.input_number.text)
+            return value
+        except ValueError:
+            return 0
 
 ConvertMiles().run()
